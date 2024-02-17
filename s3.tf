fb06835d6e69650835cf09ec0168c5e6d7e6518f
Alves0611 <g.alves0611@gmail.com>
2024-02-17 14:33:46 -0300

    adding bucket policy and creating bucket for logs

--- s3.tf
+++ s3.tf
@@ -2,5 +2,36 @@ module "website_bucket" {
   source = "github.com/Alves0611/terraform-aws-s3-object"
 
   name     = local.domain_name
-  filepath = "${path.module}/../website/dist"
+  filepath = "${path.module}/website/dist"
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = module.website_bucket.name
+  policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Id" : "PolicyForCloudFrontPrivateContent",
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontServicePrincipal",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${local.domain_name}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : "arn:aws:cloudfront::${local.account_id}:distribution/${aws_cloudfront_distribution.this.id}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+module "logs_bucket" {
+  source        = "github.com/Alves0611/terraform-aws-s3-object"
+  name          = "logs.${local.domain_name}"
+  acl           = "log-delivery-write"
+  force_destroy = true
 }
