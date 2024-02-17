f53c1a1ef48b0f0db062c9caa55efd6750e11db8
Alves0611 <g.alves0611@gmail.com>
2024-02-17 14:16:34 -0300

    random added

--- versions.tf
+++ versions.tf
@@ -1,10 +1,12 @@
 terraform {
-  required_version = "~> 1.6"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.22"
+      version = "5.29.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5"
     }
   }
 }
