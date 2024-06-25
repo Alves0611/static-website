ca8d0909eacddcf420a610f3767411a959d2eb80
Alves0611 <g.alves0611@gmail.com>
2024-06-25 17:19:52 -0300

    feat: update locals with AWS configuration

--- locals.tf
+++ locals.tf
@@ -1,4 +1,8 @@
 locals {
-  has_domain  = var.domain_name != null
-  domain_name = local.has_domain ? var.domain_name : random_pet.website.id
+  account_id      = data.aws_caller_identity.current.account_id
+  has_domain      = var.domain_name != null
+  domain_name     = local.has_domain ? var.domain_name : random_pet.website.id
+  regional_domain = "${local.domain_name}.s3.${var.aws_region}.amazonaws.com"
+
+  distribution_id = "E2F16ALWFZXHBJ"
 }
