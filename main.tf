b15d728f50116fc542d15fc3236eb5595aa2f6f3
Alves0611 <g.alves0611@gmail.com>
2024-06-25 17:48:54 -0300

    feat: configure AWS provider in us-east-1 for ACM validation

--- main.tf
+++ main.tf
@@ -2,9 +2,15 @@ provider "aws" {
   region = var.aws_region
 
   default_tags {
-    tags = {
-      "Module"     = "Static website"
-      "ManagedBy"  = "Terraform"
-    }
+    tags = local.common_tags
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us_east_1"
+
+  default_tags {
+    tags = local.common_tags
   }
 }
